040e750793d21328e332914480167a86902be94d
Arukshayashmi <arukshayashmi@gmail.com>
2024-03-19 18:35:13 +0530

    product model update

--- controllers/productLikesController.js
+++ controllers/productLikesController.js
@@ -5,18 +5,28 @@ import { getDataUri } from "../utils/features.js";
 import cloudinary from "cloudinary"
 
 export const getAllLikedProducts = async (req, res) => {
-    console.log("Kavindu")
     try {
-        const user = await userModel.findById(req.user._id)
-        const userId = user._id
-        console.log(userId)
+        const user = await userModel.findById(req.user._id);
+        const userId = user._id;
+        
         const likedProducts = await productLikesModel.find({ user: userId, status: 1 }).populate('product');
+        
+        const likedProductIds = likedProducts.map(likedProduct => likedProduct.product._id.toString());
+        
+        const allProducts = await productModel.find();
+        
+        const productsWithFavouriteStatus = allProducts.map(product => {
+            const isFavourite = likedProductIds.includes(product._id.toString());
+            return { ...product.toObject(), isFavourite }; 
+        });
 
+        const filteredProducts = productsWithFavouriteStatus.filter(product => product.isFavourite);
+        
         res.status(200).send({
             success: true,
             message: 'All products liked by user fetched successfully',
-            totalLikedProducts: likedProducts.length,
-            likedProducts
+            totalLikedProducts: filteredProducts.length,
+            likedProducts: filteredProducts
         });
     } catch (error) {
         console.log(error);
