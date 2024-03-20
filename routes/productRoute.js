6c0c2fbcc6839a07cb3247d1de43074976dc4873
Arukshayashmi <arukshayashmi@gmail.com>
2024-03-20 22:10:01 +0530

    clear and final

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProductDetailsController, uploadProductPictureController, getAllGuestProductController, likeOrUnlikeProductController, deleteProductController, deleteProductIamgeController, getAllProductController, getSingleProductController, updateProductController, updateProductImageController } from "../controllers/productController.js";
+import { addProductDetailsController, uploadProductPictureController, getAllGuestProductController, likeOrUnlikeProductController, deleteProductController, getAllProductController, getSingleProductController } from "../controllers/productController.js";
 import { singlUpload } from "../middlewares/multer.js";
 import { isAuthentic } from "../middlewares/authMiddleware.js";
 
@@ -14,23 +14,11 @@ router.get('/get-all-guest', getAllGuestProductController)
 //Get Single product
 router.get('/:id', getSingleProductController)
 
-//create New Product
-// router.post('/create', singlUpload, isAuthentic, createNewProductController)
-
 // product image upload
 router.put('/upload-picture',singlUpload, isAuthentic, uploadProductPictureController);
 
 // add product details
 router.post('/add-details',isAuthentic, addProductDetailsController);
-
-//update product
-router.put('/:id', isAuthentic, updateProductController)
-
-//update product image
-router.put('/image/:id', isAuthentic, singlUpload, updateProductImageController)
-
-//delete product image
-router.delete('/delete-image/:id', isAuthentic, deleteProductIamgeController)
 
 //delete product
 router.delete('/delete/:id', isAuthentic, deleteProductController)
